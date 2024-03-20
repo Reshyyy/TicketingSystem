b2d2abbbc591f779780ca7a21425bc4bc21a5886
Reshyyy <riyadhbrailleanthonyligan@gmail.com>
2024-03-20 14:32:52 +0800

    fix

--- src/components/header/NavbarMain.jsx
+++ src/components/header/NavbarMain.jsx
@@ -4,25 +4,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import rafiNavbarLogo from './../../assets/rafiLogo.png'
-import { Stack } from 'react-bootstrap';
+import { Col, Stack } from 'react-bootstrap';
 
 const NavbarMain = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
+            <Navbar expand="lg" className="">
                 {/* <Container> */}
                     <Stack>
                         <img src={rafiNavbarLogo} style={{ height: '60px', width: '100px' }} alt="navbarLogo" />
                     </Stack>
-                    <Stack>
+                    <Stack className='d-flex flex-column justify-content-start'>
                         <h6>Ticketing System</h6>
                         <h6>RAFI - Ramon Aboitiz Foundation Inc.</h6>
                     </Stack>
-
-
-
-
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
                 {/* <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="/dashboard">Dashboard</Nav.Link>
